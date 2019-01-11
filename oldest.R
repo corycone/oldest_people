15e412c5a0d2a82f3c9682a6d9b48667dadd5887
Cory Cone <ccone@mica.edu>
2019-01-11 15:43:05 -0500

    some comments

--- oldest.R
+++ oldest.R
@@ -5,6 +5,7 @@ data <- read.xlsx("data/oldest_working.xlsx", sheetName = "Sheet1")
 
 plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
   geom_point(size = 3, color = "#be94be") +
+  #the below geom_text looks terrible. May try to fix later on, so leaving code for now.
   #geom_text(aes(label=Age), size = 2, hjust=.45, vjust=-3, alpha = .8, color = "#64a164") +
   geom_segment( aes(x=Name, xend=Name, y=108, yend=Age), color = "#be94be") +
   theme_light() +
@@ -18,7 +19,7 @@ plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
         axis.ticks.x = element_blank(),
         panel.grid = element_blank(),
         legend.position="none",
-        #axis.text.x = element_blank(),
+        #axis.text.x = element_blank(), #leaving this here temporarily.
         plot.margin = margin(1,1,1,1, "cm"))
           
 
