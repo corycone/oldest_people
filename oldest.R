4a6694444e51888474503c253a8a8eafe334c957
Cory Cone <incognitocory@gmail.com>
2019-01-10 22:18:13 -0500

    Color test (#be94be)

--- oldest.R
+++ oldest.R
@@ -4,15 +4,15 @@ data <- read.xlsx("data/oldest_working.xlsx", sheetName = "Sheet1")
 
 
 plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
-  geom_point(size = 3, color = "Skyblue") +
-  #geom_text(aes(label=Died), size = 2, hjust=.35, vjust=-1) +
-  geom_segment( aes(x=Name, xend=Name, y=108, yend=Age), color = "Skyblue") +
+  geom_point(size = 3, color = "#be94be") +
+  #geom_text(aes(label=Age), size = 2, hjust=.45, vjust=-3, alpha = .8, color = "#64a164") +
+  geom_segment( aes(x=Name, xend=Name, y=108, yend=Age), color = "#be94be") +
   theme_light() +
   ylim(108,123) +
   labs(title = "How old were the oldest people in the world at time of death?", 
        x = "Name", 
        y = "Age") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8, color = "#be94be")) +
   theme(panel.grid.major.x = element_blank(),
         panel.border = element_blank(),
         axis.ticks.x = element_blank(),
