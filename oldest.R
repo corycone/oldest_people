2254ca1ba4947fa347a3700da7c4b1a74c0329a9
Cory Cone <ccone@mica.edu>
2019-01-14 10:07:07 -0500

    moved scale color to bottom.

--- oldest.R
+++ oldest.R
@@ -6,13 +6,9 @@ data <- read.xlsx("data/Oldest_Person_Titleholders.xlsx", sheetName = "Sheet1")
 
 plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
   geom_point(aes(color = Sex), size = 1.8) +
-  scale_colour_brewer(palette = "Set2") +
-  #geom_text for age at end of segment.
   geom_text(aes(label=paste(format(Age),"years"),vjust = .25, hjust = -.2), size = 2, alpha = 1, color = "#151515") +
-  #geom_text to add a row for date of death next to the name.
   geom_text(aes(label=ifelse(Died == "N/A", "still living ", paste("died:", format(Died))), vjust = .55, hjust = .5, y=107), size = 2, alpha = 1, color = "#151515") +
   geom_segment( aes(x=Name, xend=Name, y=108, yend=Age, color = Sex), size = 1) +
-  #scale_colour_brewer(palette = "Set1") +
   theme_light() +
   ylim(107,123) +
   labs(title = "World's Oldest Person Titleholders Since 1955", 
@@ -32,6 +28,7 @@ plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
         plot.caption=element_text(size = 7, color = "#151515"),
         legend.position="right",
         plot.margin = margin(1,1,1,1, "cm")) +
+  scale_colour_brewer(palette = "Set2") +
   coord_flip()
           
 
