a783c26f44d62b2e23c7a51853100c7037751328
Cory Cone <incognitocory@gmail.com>
2019-01-13 20:18:33 -0500

    if else for still living or died.

--- oldest.R
+++ oldest.R
@@ -1,15 +1,15 @@
 library(xlsx)
 library(ggplot2)
 
-data <- read.xlsx("data/oldest_working.xlsx", sheetName = "Sheet1")
+data <- read.xlsx("data/Oldest_Person_Titleholders.xlsx", sheetName = "Sheet1")
 
 
 plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
   geom_point(size = 1.7, color = "Gray") +
   #geom_text for age at end of segment.
-  geom_text(aes(label=paste(format(Age, digits=2, nsmall=2),"years"),vjust = .17, hjust = -.2), size = 2, alpha = 1, color = "#151515") +
-  #geom_text to add a row for date of death next to the name. I can't figure out how to get the NA to not be centered.
-  geom_text(aes(label=paste("d.", format(Died, digits=2, nsmall=2)),vjust = .6, hjust = .6, y=107), size = 2, alpha = 1, color = "#151515") +
+  geom_text(aes(label=paste(format(Age),"years"),vjust = .17, hjust = -.2), size = 2, alpha = 1, color = "#151515") +
+  #geom_text to add a row for date of death next to the name.
+  geom_text(aes(label=ifelse(Died == "N/A", "still living ", paste("died:", format(Died))), vjust = .6, hjust = .5, y=107), size = 2, alpha = 1, color = "#151515") +
   geom_segment( aes(x=Name, xend=Name, y=108, yend=Age), color = "Gray", size = 1) +
   theme_light() +
   ylim(107,123) +
@@ -17,8 +17,8 @@ plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
        x = "Titleholder", 
        y = "Age of Death",
        caption = "Source: Gerontology Research Group - http://www.grg.org/Adams/C.HTM") +
-    theme(axis.text.x = element_text(angle = 0, hjust = 1, color = "#151515"),
-        axis.text.y = element_text(angle = 0, hjust = 1, size = 8, color = "#151515"),
+    theme(axis.text.x = element_text(angle = 0, hjust = 1, color = "#151515", size = 7),
+        axis.text.y = element_text(angle = 0, hjust = 1, size = 7, color = "#151515"),
         panel.grid.major.x = element_blank(),
         panel.border = element_blank(),
         axis.ticks.x = element_blank(),
@@ -33,3 +33,4 @@ plot <- ggplot(data, aes(x = reorder(Name, X.), Age)) +
           
 
 plot
+
